6a39da06a4db7c5dbdd592a465ae4fa6cfb10c2c
Javier Ortiz <je.ortiz@mesfix.com>
2020-11-18 15:41:49 -0600

    prueba2

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 let axios = require('axios').default
-let heaaders ={ "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-                "Accept-Encoding":	"gzip, deflate, br",
-                "Accept-Language" :	"es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
-                "Connection":	"keep-alive",
-                "Host"	:"api.github.com",
-                "Upgrade-Insecure-Requests":	1}
 
-axios.get(process.env.COMMITS,{headers:heaaders})
-.then(data=>{console.log(JSON.stringify(data.data));})
+axios.get(process.env.COMMITS)
+.then(data=>{
+        let dataJson =  JSON.parse(data.data);
+        console.log(dataJson);
+    })
 .catch(err=>{console.log(err);})
