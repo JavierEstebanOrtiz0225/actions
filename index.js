4ab04356ce0f495ff3ec4054c1e791bda943d8dc
Javier Ortiz <je.ortiz@mesfix.com>
2020-11-18 18:09:05 -0600

    prueba2

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 let axios = require('axios').default
 
+// axios.get(process.env.COMMITS)
+// .then(data=>{console.log(data.data[data.data.length-1]);})
+// .catch(err=>{console.log(err);})
+
+
 console.log('-----------------------------------');
-console.log(process.env.VAR);
+console.log(process.env.ACTOR);
 console.log('-----------------------------------');
